6920c8bf5078063f6e5adb99197646679fd1845b
Oleksandr Pronin <pronin.alx@gmail.com>
2024-02-17 13:26:15 +0100

    feat: prepare and train model, estimate it after

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 import os
 import shutil
+import matplotlib.pyplot as plt
+
+# from tensorflow.keras import layers, models, optimizers
 
 # import numpy as np
 # import sklearn
@@ -29,11 +32,96 @@ def load_dataset():
 def main():
     data_dir = load_dataset()
     print(f"data_dir: {data_dir}")
-    # project_dir = os.path.dirname(os.path.abspath(__file__))
-    # print(f"project_dir: {project_dir}")
-    # print(np.__version__)
-    # print(tf.__version__)
-    # print(sklearn.__version__)
+    # Define the image size and batch size
+    img_size = (160, 160)
+    batch_size = 32
+    # Load the training and validation datasets
+    train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=img_size,
+        batch_size=batch_size,
+    )
+    validation_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=img_size,
+        batch_size=batch_size,
+    )
+    # print(train_dataset)
+    # print(validation_dataset)
+
+    # Configure the Dataset for Performance
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+    validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+
+    # Load the Pre-trained Model
+    pretrained_model = tf.keras.applications.MobileNetV2(
+        input_shape=(160, 160, 3), include_top=False, weights="imagenet"
+    )
+
+    # Freeze the Convolutional Base
+    pretrained_model.trainable = False
+
+    # Add a Classification Head
+    model = tf.keras.models.Sequential(
+        [
+            pretrained_model,
+            tf.keras.layers.GlobalAveragePooling2D(),
+            tf.keras.layers.Dense(1)
+        ]
+    )
+
+    # Compile the Model
+    model.compile(
+        optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.0001),
+        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
+
+    # Train the Model
+    history = model.fit(
+        train_dataset,
+        epochs=10,
+        validation_data=validation_dataset
+    )
+
+    # Evaluate the Model
+    acc = history.history["accuracy"]
+    val_acc = history.history["val_accuracy"]
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(acc, label="Training Accuracy")
+    plt.plot(val_acc, label="Validation Accuracy")
+    plt.legend(loc="lower right")
+    plt.ylabel("Accuracy")
+    plt.ylim([min(plt.ylim()), 1])
+    plt.title("Training and Validation Accuracy")
+
+    plt.subplot(2, 1, 2)
+    plt.plot(loss, label="Training Loss")
+    plt.plot(val_loss, label="Validation Loss")
+    plt.legend(loc="upper right")
+    plt.ylabel("Cross Entropy")
+    plt.ylim([0, 1.0])
+    plt.title("Training and Validation Loss")
+    plt.xlabel("epoch")
+    plt.show()
+
+
+# project_dir = os.path.dirname(os.path.abspath(__file__))
+# print(f"project_dir: {project_dir}")
+# print(np.__version__)
+# print(tf.__version__)
+# print(sklearn.__version__)
 
 
 if __name__ == "__main__":
